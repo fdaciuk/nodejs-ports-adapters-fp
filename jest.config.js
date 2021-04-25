8cda0f5525cca0a2049bea1432a8f778b3a5fbf1
Fernando Daciuk <f.daciuk@gmail.com>
2021-04-24 21:50:29 -0300

    Ignore type-checking when run tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@
 
 module.exports = {
   preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
