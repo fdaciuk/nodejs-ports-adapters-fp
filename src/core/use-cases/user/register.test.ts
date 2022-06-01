d68575ea278f58e371574410265ffa3c7d1767ec
Rharison Lucas <rharison.abreu@gmail.com>
2022-06-01 07:54:27 -0300

    Comments.

--- src/core/use-cases/user/register.test.ts
+++ src/core/use-cases/user/register.test.ts
@@ -1,7 +1,17 @@
 import * as TE from 'fp-ts/TaskEither'
+import * as E from 'fp-ts/Either'
 import { pipe } from 'fp-ts/function'
 import { register, OutsideRegister } from './register'
 import { CreateUser } from '../../types/user'
+
+// Usamos Either quando temos algum valor síncrono que possivelmente vai disparar um erro
+// type <Either<E, A> = Right<A> | Left<E>
+
+// Usamos Task para funções que retornam uma promise que nunca quebra.
+// type Task<A> = Lazy<Promise<A>>
+
+// Usamos o TaskEither para funções uma promise que possivelmente dispara um erro.
+// type TaskEither<E, A> = Either<E, Task<A>>
 
 const registerOk: OutsideRegister<string> = async (data) => {
   return `Usuário ${data.username} cadastrado com sucesso!`
@@ -14,6 +24,11 @@ const data: CreateUser = {
 }
 
 it('Deveria cadastrar um usuário com sucesso', async () => {
+  // const result = await register(registerOk)(data)()
+  // if(E.isRight(result)) {
+  //   expect(result.right).toBe(`Usuário ${data.username} cadastrado com sucesso!`)
+  // }
+
   return pipe(
     data,
     register(registerOk),
