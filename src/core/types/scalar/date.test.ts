4e991169729688b0b4f9db7029c8ee6f6f687a74
Rharison Lucas <rharison.abreu@gmail.com>
2022-06-01 19:29:50 -0300

    Create type and test to date.

---
+++ src/core/types/scalar/date.test.ts
@@ -0,0 +1,12 @@
+import { pipe } from 'fp-ts/function'
+import { mapAllE } from '@/config/tests/fixtures'
+import { dateCodec } from './date'
+
+it('Should validate date properly', () => {
+  const date = new Date().toISOString();
+  pipe(
+    date,
+    dateCodec.decode,
+    mapAllE(result => expect(result).toBe(date)),
+  )
+})
