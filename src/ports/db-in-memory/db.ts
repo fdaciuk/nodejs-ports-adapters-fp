457d43fed498e5c27a7eec98398c713f97626d3f
Rharison Lucas <rharison.abreu@gmail.com>
2022-06-03 14:21:16 -0300

    Create port for data base in memory.

---
+++ src/ports/db-in-memory/db.ts
@@ -0,0 +1,13 @@
+import { OutsideRegisterType } from '@/adapters/use-cases/user/register-user-adapter'
+
+export const outsideRegister: OutsideRegisterType = async (data) => {
+  return {
+    user: {
+      email: data.email,
+      token: '',
+      username: data.username,
+      bio: '',
+      image: undefined,
+    },
+  }
+}
