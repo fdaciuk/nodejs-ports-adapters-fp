3435e7ee18539f48dc966325f69d6e132fc5f150
Rharison Lucas <rharison.abreu@gmail.com>
2022-06-10 09:37:55 -0300

    Refactoring functions unsafe.

--- src/config/tests/fixtures/index.ts
+++ src/config/tests/fixtures/index.ts
@@ -1,24 +1,11 @@
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
-import { Email, Slug, Password } from '../../../core/types/scalar'
 
 type Callback = (a: unknown) => unknown
 type MapAll = (fn: Callback) => (data: TE.TaskEither<unknown, unknown>) => TE.TaskEither<unknown, unknown>
 
-export function unsafeEmail (value: string): Email {
-  return value as any
-}
-
-export function unsafeSlug (value: string): Slug {
-  return value as any
-}
-
-export function unsafePassword (value: string): Password {
-  return value as any
-}
-
-export function unsafeString (value: unknown): string {
-  return value as any
+export function unsafe <T>(value: unknown): T {
+  return value as T
 }
 
 export const mapAll: MapAll = (fn) => (data) => {
