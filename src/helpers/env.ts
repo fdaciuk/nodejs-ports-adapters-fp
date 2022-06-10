e1def6889575d05925c559f2d641c9bd8554f736
Rharison Lucas <rharison.abreu@gmail.com>
2022-06-10 09:39:03 -0300

    Refactoring type NonEmptyString.

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,21 +1,13 @@
-import * as t from 'io-ts'
 import * as E from 'fp-ts/Either'
 import { failure } from 'io-ts/PathReporter'
 import { pipe } from 'fp-ts/function'
-import { withMessage } from 'io-ts-types'
-
-type LenghtBrand = {
-  readonly NonEmptyString: unique symbol
-}
-
-const NonEmptyStringCodec = t.brand(
-  t.string,
-  (value): value is t.Branded<string, LenghtBrand> => isNonEmptyString(value),
-  'NonEmptyString',
-)
+import { withMessage, NonEmptyString } from 'io-ts-types'
 
 export const env = (value: string) => {
-  const envCodec = withMessage(NonEmptyStringCodec, () => `You must set the env var ${value}`)
+  const envCodec = withMessage(
+    NonEmptyString,
+    () => `You must set the env var ${value}`,
+  )
   return pipe(
     envCodec.decode(process.env[value]),
     E.fold(
@@ -23,8 +15,4 @@ export const env = (value: string) => {
       (value) => value,
     ),
   )
-}
-
-function isNonEmptyString (value: unknown) {
-  return typeof value === 'string' && value.length > 0
 }
