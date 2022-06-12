60bb9c0c684cbf25dc4e3fcb38d345449b26fea8
Rharison Lucas <rharison.abreu@gmail.com>
2022-06-12 13:03:30 -0300

    Adjuste.

--- src/adapters/use-cases/user/register-user-adapter.ts
+++ src/adapters/use-cases/user/register-user-adapter.ts
@@ -4,4 +4,4 @@ import { User } from '@/core/types/user'
 export const registerUser: user.RegisterUser = (outsideRegister) => (data) =>
   user.registerUser(outsideRegister)(data)
 
-export type OutsideRegisterUser = user.OutsideRegister<{user: User}>
+export type OutsideRegisterUser = user.OutsideRegisterUser<{user: User}>
